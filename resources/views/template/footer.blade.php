13301b5108e04cec7d9e8a85d94488214d9be93d
pHedi3 <federico.gualdi3@gmail.com>
2021-09-02 17:00:37 +0200

    problema background url

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,1 +1,51 @@
-io sono il footer
+<div class="background">
+    <div class="list">
+        <div class="in-block">
+            <ul>
+              <li>DC COMICS</li>
+              <li><a href="">Characters</a></li>
+              <li><a href="">Comics</a></li>
+              <li><a href="">Movies</a></li>
+              <li><a href="">Tv</a></li>
+              <li><a href="">Games</a></li>
+              <li><a href="">Videos</a></li>
+              <li><a href="">News</a></li>
+            </ul>
+            <ul>
+              <li>SHOP</li>
+              <li><a href="">Shop DC</a></li>
+              <li><a href="">Shop DC Collectibles</a></li>
+            </ul>
+        </div>
+        <ul class="in-block">
+            <li>DC</li>
+            <li><a href="">Terms Of Use</a></li>
+            <li><a href="">Privacy policy (New)</a></li>
+            <li><a href="">Ad Choices</a></li>
+            <li><a href="">Advertising</a></li>
+            <li><a href="">Jobs</a></li>
+            <li><a href="">Subscriptions</a></li>
+            <li><a href="">Talent Workshops</a></li>
+            <li><a href="">CPSC Certificates</a></li>
+            <li><a href="">Ratings</a></li>
+            <li><a href="">Shop Help</a></li>
+            <li><a href="">Contacts Us</a></li>
+        </ul>
+        <ul class="in-block">
+            <li>DC COMICS</li>
+            <li><a href="">Characters</a></li>
+            <li><a href="">Comics</a></li>
+            <li><a href="">Movies</a></li>
+            <li><a href="">Tv</a></li>
+            <li><a href="">Games</a></li>
+            <li><a href="">Videos</a></li>
+            <li><a href="">News</a></li>
+        </ul>
+    </div>
+    <div class="logo-footer">
+
+    </div>
+    <div class="social">
+
+    </div>
+</div>
