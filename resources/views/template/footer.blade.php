e6bbbdf3d664fb3fba7b841b4fb458a88997eb0d
pHedi3 <federico.gualdi3@gmail.com>
2021-09-02 17:43:11 +0200

    fine footer e header

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,51 +1,63 @@
 <div class="background">
-    <div class="list">
-        <div class="in-block">
-            <ul>
-              <li>DC COMICS</li>
-              <li><a href="">Characters</a></li>
-              <li><a href="">Comics</a></li>
-              <li><a href="">Movies</a></li>
-              <li><a href="">Tv</a></li>
-              <li><a href="">Games</a></li>
-              <li><a href="">Videos</a></li>
-              <li><a href="">News</a></li>
+    <div>
+
+        <div class="list">
+            <div class="in-block">
+                <ul>
+                  <li>DC COMICS</li>
+                  <li><a href="">Characters</a></li>
+                  <li><a href="">Comics</a></li>
+                  <li><a href="">Movies</a></li>
+                  <li><a href="">Tv</a></li>
+                  <li><a href="">Games</a></li>
+                  <li><a href="">Videos</a></li>
+                  <li><a href="">News</a></li>
+                </ul>
+                <ul>
+                  <li>SHOP</li>
+                  <li><a href="">Shop DC</a></li>
+                  <li><a href="">Shop DC Collectibles</a></li>
+                </ul>
+            </div>
+            <ul class="in-block">
+                <li>DC</li>
+                <li><a href="">Terms Of Use</a></li>
+                <li><a href="">Privacy policy (New)</a></li>
+                <li><a href="">Ad Choices</a></li>
+                <li><a href="">Advertising</a></li>
+                <li><a href="">Jobs</a></li>
+                <li><a href="">Subscriptions</a></li>
+                <li><a href="">Talent Workshops</a></li>
+                <li><a href="">CPSC Certificates</a></li>
+                <li><a href="">Ratings</a></li>
+                <li><a href="">Shop Help</a></li>
+                <li><a href="">Contacts Us</a></li>
             </ul>
-            <ul>
-              <li>SHOP</li>
-              <li><a href="">Shop DC</a></li>
-              <li><a href="">Shop DC Collectibles</a></li>
+            <ul class="in-block">
+                <li>DC COMICS</li>
+                <li><a href="">Characters</a></li>
+                <li><a href="">Comics</a></li>
+                <li><a href="">Movies</a></li>
+                <li><a href="">Tv</a></li>
+                <li><a href="">Games</a></li>
+                <li><a href="">Videos</a></li>
+                <li><a href="">News</a></li>
             </ul>
         </div>
-        <ul class="in-block">
-            <li>DC</li>
-            <li><a href="">Terms Of Use</a></li>
-            <li><a href="">Privacy policy (New)</a></li>
-            <li><a href="">Ad Choices</a></li>
-            <li><a href="">Advertising</a></li>
-            <li><a href="">Jobs</a></li>
-            <li><a href="">Subscriptions</a></li>
-            <li><a href="">Talent Workshops</a></li>
-            <li><a href="">CPSC Certificates</a></li>
-            <li><a href="">Ratings</a></li>
-            <li><a href="">Shop Help</a></li>
-            <li><a href="">Contacts Us</a></li>
-        </ul>
-        <ul class="in-block">
-            <li>DC COMICS</li>
-            <li><a href="">Characters</a></li>
-            <li><a href="">Comics</a></li>
-            <li><a href="">Movies</a></li>
-            <li><a href="">Tv</a></li>
-            <li><a href="">Games</a></li>
-            <li><a href="">Videos</a></li>
-            <li><a href="">News</a></li>
-        </ul>
+        <span>All Site Content TM and  c 2020 DC Entertainment, unless otherwise <a class="blue-text" href="">noted here.</a>. All rights reserved.<br><a class="blue-text" href="">Cokies Settings</a></span>
     </div>
     <div class="logo-footer">
-
+        <img src="./images/dc-logo-bg.png" alt="">
     </div>
-    <div class="social">
-
+</div>
+<div class="social">
+    <button>SING-UP NOW!</button>
+    <div class="link-social">
+        <a class="blue-text" href="">FOLLOW US</a>
+        <img src="./images/footer-facebook.png" alt="">
+        <img src="./images/footer-twitter.png" alt="">
+        <img src="./images/footer-youtube.png" alt="">
+        <img src="./images/footer-pinterest.png" alt="">
+        <img src="./images/footer-periscope.png" alt="">
     </div>
 </div>
