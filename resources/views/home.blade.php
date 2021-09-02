baee330bec2c3ca64c145f308f67d480f28713d4
pHedi3 <federico.gualdi3@gmail.com>
2021-09-02 16:40:55 +0200

    inizio lavoro

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('template.base')
 @section('title-tab', 'HOME' )
 @section('main')
+<a href="{{route('prodotto-1')}}">vai al prodotto 1</a>
     sono il main di home
 @endsection
--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -1,2 +1,24 @@
-sono la header 
-<a href="{{route('prodotto-1')}}">vai al prodotto 1</a>
+<div class="blue">
+    <span>DC POWER<span class="litle">SM</span> VISA</span>
+    <span class="last">ADDITIONAL DC SITE</span>
+</div>
+<div class="back-nav">
+    <div class="logo">
+        <img src="./images/dc-logo.png" alt="">
+    </div>
+    <nav>
+        <ul>
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+        </ul>
+    </nav>
+    <input type="text" name="" id="">
+</div>
